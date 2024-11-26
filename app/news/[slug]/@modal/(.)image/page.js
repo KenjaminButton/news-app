e7baebadf65b55cd0733cf7e64e18c654ebace2c
Kenjamin Button <kennethpchang@gmail.com>
2024-11-26 14:25:25 -0800

    Modal closes with useRouter programatically going back a page with an onClick handler

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,12 @@
+"use client"
+
 import { DUMMY_NEWS } from '@/dummy-news'
-import { notFound } from 'next/navigation'
+import { notFound, useRouter } from 'next/navigation'
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter()
+  
+
   const newsItemSlug = params.slug
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemSlug)
 
@@ -12,7 +17,7 @@ export default function InterceptedImagePage({ params }) {
   return (
     <>
       {/* <h2>Intercepted Page</h2> */}
-      <div className='modal-backdrop' />
+      <div className='modal-backdrop' onClick={router.back} />
         <dialog className='modal' open>
           <div className='fullscreen-image'>
             <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
